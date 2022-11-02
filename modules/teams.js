a89885f7daad93dc5d16612146dd867c2ec3e895
giacomotolari <tolarigiacomo@gmail.com>
2022-11-02 09:08:51 +0100

    firs comitt

---
+++ modules/teams.js
@@ -0,0 +1,36 @@
+//named exports
+
+export const teamA = {
+  teamName: "teamA",
+  players: [
+    { firstName: "mike", lastName: "smith", age: 20 },
+    { firstName: "sarah", lastName: "smith", age: 24 },
+  ],
+};
+
+export const teamB = {
+  teamName: "teamB",
+  players: [
+    { firstName: "hugo", lastName: "james", age: 21 },
+    { firstName: "laura", lastName: "uru", age: 22 },
+  ],
+};
+
+export const teamC = {
+  teamName: "teamC",
+  players: [
+    { firstName: "mike", lastName: "smith", age: 18 },
+    { firstName: "sarah", lastName: "james", age: 30 },
+  ],
+};
+
+export const teamD = {
+  teamName: "teamD",
+  players: [
+    { firstName: "jack", lastName: "ionu", age: 29 },
+    { firstName: "matt", lastName: "otto", age: 40 },
+  ],
+};
+
+export const allTeams = [teamA, teamB, teamC, teamD];
+
